5e4a3552dce52413ede244d28f661e076849703c
Zheng He Hu <aeonzh@hotmail.com>
2023-07-31 18:42:07 +0100

    Set 8 char lenght password
    This is required by the `/api/auth/email-login` endpoint
    
    The previously seeded password will fail to login

--- packages/db/setup.sh
+++ packages/db/setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-psql --host $PG_HOST -U $PG_USER -d $PG_DB -c "CREATE USER app_user WITH PASSWORD 'app_pass';"  || true
+psql --host $PG_HOST -U $PG_USER -d $PG_DB -c "CREATE USER app_user WITH PASSWORD 'app_pass';"
 echo "created app_user"
 yarn workspace @omnivore/db migrate
-psql --host $PG_HOST -U $PG_USER -d $PG_DB -c "GRANT omnivore_user TO app_user;"  || true
+psql --host $PG_HOST -U $PG_USER -d $PG_DB -c "GRANT omnivore_user TO app_user;"
 echo "granted omnivore_user to app_user"
 
-# create demo user with email: demo@omnivore.app, password: demo
+# create demo user with email: demo@omnivore.app, password: demodemo
 USER_ID=$(uuidgen)
-PASSWORD='$2a$10$nxdWohvkWWpmQ28aTq0BK./pRgt6/WDrWyArvod8uWqxpFu/cfVoy'
+PASSWORD='$2a$10$mNsL6PYHBMtITU6HktpbLOU.pYohiytmNwnVdeYUSpKUv61.EUdL.'
 psql --host $PG_HOST -U $PG_USER -d $PG_DB -c "INSERT INTO omnivore.user (id, source, email, source_user_id, name, password) VALUES ('$USER_ID', 'EMAIL', 'demo@omnivore.app', 'demo@omnivore.app', 'Demo User', '$PASSWORD'); INSERT INTO omnivore.user_profile (user_id, username) VALUES ('$USER_ID', 'demo');"
-echo "created demo user with email: demo@omnivore.app, password: demo"
+echo "created demo user with email: demo@omnivore.app, password: demodemo"
